8c1af8142b23a2ac8644a94faa7efc4d57da2af9
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2022-11-03 12:24:45 -0700

    feat: Use GraphQL query to populate zones
    Adds action for initialization
    
    ref: PUDL-374

--- apps/pudl/src/store/index.ts
+++ apps/pudl/src/store/index.ts
@@ -1,10 +1,32 @@
+import { query } from '@pbotapps/common';
 import { defineStore } from 'pinia';
+
+interface Zone {
+  name: string;
+  schemas: Array<Schema>;
+}
+
+interface Schema {
+  name: string;
+  tables: Array<Table>;
+}
+
+interface Table {
+  name: string;
+  columns: Array<Column>;
+}
+
+interface Column {
+  name: string;
+  type: string;
+}
 
 export const useStore = defineStore('pudl', {
   state: () => ({
     data: {
-      findZone: [],
+      findZone: [] as Array<Zone>,
     },
+    errors: [] as Array<unknown>,
   }),
   getters: {
     zones: state => state.data.findZone,
@@ -14,5 +36,36 @@ export const useStore = defineStore('pudl', {
       state.data.findZone
         .find(z => z.name == zone)
         ?.schemas.find(s => s.name == schema)?.tables,
+  },
+  actions: {
+    async getZones() {
+      try {
+        const res = await query<{ findZone: Array<Zone> }>({
+          operation: `query FindZone {
+          findZone {
+            name
+            schemas {
+              name
+              description
+              tables {
+                name
+                columns {
+                  name
+                  type
+                  index
+                }
+              }
+            }
+          }
+        }`,
+        });
+
+        if (res.data) {
+          this.data = res.data;
+        }
+      } catch (err) {
+        this.errors.push(err);
+      }
+    },
   },
 });
