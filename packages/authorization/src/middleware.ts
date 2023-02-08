42a704e8062d57ae987576ae4620237abee3502f
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2023-02-08 01:52:40 +0000

    fix: Token parsing and error handling

--- packages/authorization/src/middleware.ts
+++ packages/authorization/src/middleware.ts
@@ -1,12 +1,11 @@
 import { RequestHandler } from 'express';
-import { createRemoteJWKSet, jwtVerify } from 'jose';
+import { createRemoteJWKSet, JWTPayload, jwtVerify } from 'jose';
 import fetch from 'node-fetch';
-import { UserRepository } from './repository.js';
 
 export const handleToken =
   ({ fail = true }): RequestHandler =>
   async (req, res, next) => {
-    const token = req['token'];
+    const token = req['token'] || req.header('Authorization').split(' ')[1];
 
     if (!token && fail) {
       res.status(401);
@@ -25,30 +24,38 @@ export const handleToken =
         .then(res => res.json())
         .then((json: { jwks_uri: string }) => json.jwks_uri);
 
-      const { payload } = await jwtVerify(
-        token,
-        createRemoteJWKSet(new URL(await jwksURI)),
-        {
-          issuer: `https://sts.windows.net/${process.env.AZURE_TENANT_ID}/`,
-          audience: process.env.AZURE_CLIENT_ID,
-        }
-      );
-
-      req['token'] = payload;
+      try {
+        const { payload } = await jwtVerify(
+          token,
+          createRemoteJWKSet(new URL(await jwksURI)),
+          {
+            issuer: `https://sts.windows.net/${process.env.AZURE_TENANT_ID}/`,
+            audience: process.env.AZURE_CLIENT_ID,
+          }
+        );
+        req['token'] = payload;
+      } catch {
+        res.status(401);
+        res.send({
+          error: 'UNAUTHORIZED',
+          message: 'Invalid token!',
+        });
+        return;
+      }
     }
 
     next();
   };
 
-export const handleUser =
-  (repository: UserRepository): RequestHandler =>
-  async (req, _, next) => {
-    const token = req['token'];
+export const handleUser = (): RequestHandler => async (req, _, next) => {
+  const token = req['token'] as JWTPayload;
 
-    if (token) {
-      // Presume this to be verified and replaced with decrypted payload
-      req.user = await repository.getUser({ oauthId: token['oid'] });
-    }
+  if (token) {
+    // Presume this to be verified and replaced with decrypted payload
+    req['user'] = {
+      _id: token.sub,
+    };
+  }
 
-    next();
-  };
+  next();
+};
