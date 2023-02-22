8d96065f7bd9530d439fa74712c3953d1aa5a53c
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2023-02-22 05:24:26 +0000

    feat: Add Missing image component

---
+++ apps/sign-library/src/components/MissingImage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Box } from '@pbotapps/components';
+</script>
+
+<template>
+  <Box
+    color="gray"
+    variant="light"
+    border
+    rounded
+    class="flex justify-center items-center text-center p-4"
+  >
+    Missing sign image
+  </Box>
+</template>
