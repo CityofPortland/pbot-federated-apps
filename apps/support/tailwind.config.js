67d3abc702ac539775db64ce90886669262d6c68
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2023-09-05 14:27:46 -0700

    fix: Look for markdown styling from support app
    ref: SOS-284

--- apps/support/tailwind.config.js
+++ apps/support/tailwind.config.js
@@ -2,4 +2,11 @@ const defaultConfig = require('../../tailwind.config');
 
 module.exports = {
   ...defaultConfig,
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx,md}',
+    './docs/**/*.{vue,ts,md}',
+    '../../packages/components/src/**/*.{vue,js,ts,jsx,tsx,md}',
+    '../../services/support/src/**/*.{vue,js,ts,jsx,tsx,md}',
+  ],
 };
