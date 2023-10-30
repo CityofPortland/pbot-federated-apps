e89f4c2f68fbb1fb0f73e5f335f6b8605ce41d06
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2023-10-30 14:42:36 -0700

    fix: Add incremental query check
    Fixes infinite routing loop possibility while applying several defaults

--- apps/sign-library/src/router/index.ts
+++ apps/sign-library/src/router/index.ts
@@ -1,10 +1,27 @@
 import { authRoutes } from '@pbotapps/components';
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import {
+  createRouter,
+  createWebHistory,
+  RouteLocationNormalized,
+  RouteRecordRaw,
+} from 'vue-router';
 
 import Edit from '../pages/Edit.vue';
 import Home from '../pages/Home.vue';
 import Revisions from '../pages/Revisions.vue';
 import View from '../pages/View.vue';
+
+const checkQuery =
+  (prop: string, def: string | number) => (to: RouteLocationNormalized) => {
+    if (!to.query[prop])
+      return {
+        ...to,
+        query: {
+          ...to.query,
+          [prop]: def,
+        },
+      };
+  };
 
 const routes: RouteRecordRaw[] = [
   {
@@ -23,20 +40,13 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/search',
     component: Home,
-    beforeEnter(to) {
-      if (!to.query) {
-        return {
-          ...to,
-          query: {
-            status: 'in_use',
-            pageSize: 10,
-            page: 1,
-            sort: '_changed',
-            sortOrder: 'asc',
-          },
-        };
-      }
-    },
+    beforeEnter: [
+      checkQuery('status', 'in_use'),
+      checkQuery('pageSize', 10),
+      checkQuery('page', 1),
+      checkQuery('sort', '_changed'),
+      checkQuery('sortOrder', 'asc'),
+    ],
   },
   { path: '/:code', component: View, props: true },
   { path: '/:code/edit', component: Edit, props: true },
@@ -48,9 +58,11 @@ const routes: RouteRecordRaw[] = [
 export default createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-  scrollBehavior() {
-    return {
-      top: 0,
-    };
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
   },
 });
