45a500e4bad14e93379673dfee9e22dfb673193f
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2024-02-23 10:27:05 -0800

    featL Add pipelines to PUDL GraphQL server
    Adds github env chart hooks to gather credentials from secrets
    Adds pipelines function to GraphQL server to download all pipeline information
    
    ref: PUDL-457

---
+++ services/pudl/src/pipeline/types.ts
@@ -0,0 +1,57 @@
+import { DocumentNode } from 'graphql';
+import gql from 'graphql-tag';
+
+export const typeDefs: DocumentNode = gql`
+  extend type Query {
+    pipeline(id: String!): Pipeline
+    pipelines: [Pipeline]
+  }
+
+  type Pipeline {
+    id: String!
+    tags: [String!]
+    schedule: String
+    lastRun: PipelineRun
+    allRuns: [PipelineRun!]
+  }
+
+  enum PipelineStatus {
+    failed
+    none
+    running
+    success
+  }
+
+  type PipelineRun {
+    id: String!
+    status: PipelineStatus!
+    startTime: DateTime!
+    endTime: DateTime
+  }
+
+  extend type Table {
+    pipelines: [Pipeline]
+  }
+`;
+
+enum PipelineStatus {
+  'failed',
+  'success',
+}
+
+export type PipelineRun = {
+  id: string;
+  status: PipelineStatus;
+  startTime: Date;
+  endTime?: Date;
+};
+
+export type Pipeline = {
+  id: string;
+  schedule?: string;
+  tags?: Array<string>;
+  lastRun?: PipelineRun;
+  allRuns?: Array<PipelineRun>;
+};
+
+export default typeDefs;
