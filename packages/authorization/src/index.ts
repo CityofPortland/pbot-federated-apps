9854afe31cd4963565bec03aef6bbe7ceda9fd3d
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2024-03-27 20:28:16 +0000

    feat: Add composable and store for auth
    ref: VERT-917

--- packages/authorization/src/index.ts
+++ packages/authorization/src/index.ts
@@ -1,3 +1,6 @@
+export * from './composable.js';
 export * from './middleware.js';
 export * from './repository.js';
 export * from './rule.js';
+export * from './store.js';
+export * from './types.js';
