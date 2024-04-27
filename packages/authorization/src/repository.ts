65719838c5f57f4a7b624005e969bb1d46fe4ee0
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2024-04-26 21:49:49 -0700

    fix: Import file extension

--- packages/authorization/src/repository.ts
+++ packages/authorization/src/repository.ts
@@ -1,5 +1,5 @@
 import { BaseType, User } from '@pbotapps/objects';
-import { RuleType } from './rule';
+import { RuleType } from './rule.js';
 
 export interface UserRepository {
   getUser(user: Partial<User>): Promise<User | undefined>;
