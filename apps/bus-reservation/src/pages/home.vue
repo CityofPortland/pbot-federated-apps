8e64c5b664b22c721a54806c5c23aac7794c9ccf
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2024-04-29 15:53:57 -0700

    nit: Change homepage layout

--- apps/bus-reservation/src/pages/home.vue
+++ apps/bus-reservation/src/pages/home.vue
@@ -6,15 +6,14 @@ import { Box } from '@pbotapps/components';
   <article class="grid grid-cols-1 gap-4">
     <Box
       as="header"
-      class="p-12 grid grid-cols-1 md:grid-cols-2 items-center justify-items-center gap-12"
+      class="p-12 grid grid-cols-1 items-center justify-items-center gap-12"
     >
       <div class="mx-auto flex flex-col gap-4 prose-lg">
         <h1>Welcome to the Bus Reservation application!</h1>
         <p>
           This application is intended for use by permitted users to validate
           that requests for exclusive use of bus parking zones have been
-          accepted by PBOT. This application will also allow permitted users to
-          privately negotiate transfers of those exclusive reservations.
+          accepted by PBOT.
         </p>
       </div>
       <div class="mx-auto grid grid-cols-3 gap-12"></div>
