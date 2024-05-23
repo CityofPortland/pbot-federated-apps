65e5cb1b40dfde87d450697374e7531ef1cfe26c
Jenny Khare <jenny.oh@portlandoregon.gov>
2024-05-23 13:23:07 -0700

    feat: Add validation when filtering dates for reservations
    ref: VERT-966

--- apps/bus-reservation/src/pages/reservations/list.vue
+++ apps/bus-reservation/src/pages/reservations/list.vue
@@ -1,16 +1,43 @@
 <script setup lang="ts">
-import { Anchor, Box } from '@pbotapps/components';
+import { Anchor, Box, Entry, Input, Message } from '@pbotapps/components';
+import { addDays, endOfDay, format, startOfDay } from 'date-fns';
 import { toZonedTime } from 'date-fns-tz';
 import { useRoute } from 'vue-router';
 import { useStore } from '../../store';
-import { onMounted } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 
 const { path } = useRoute();
 const store = useStore();
-
+const errors = ref<Error>();
 const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
 const now = toZonedTime(new Date(), tz);
+const end = ref<Date>(endOfDay(addDays(now, 30)));
+const start = ref<Date>(startOfDay(now));
+const reservations = computed(() => {
+  return store.reservations.filter(res => {
+    if (res.start <= end.value && res.end >= start.value) return true;
+  });
+});
+const validateStart = (startDt: string) => {
+  start.value = startOfDay(toZonedTime(startDt, tz));
+  errors.value = undefined;
+  if (start.value < startOfDay(toZonedTime(new Date(), tz)))
+    errors.value = Error(`Start date is before today.`);
+  else if (start.value > end.value)
+    errors.value = Error(`Start date is after end date.`);
+};
+const validateEnd = (endDt: string) => {
+  end.value = endOfDay(toZonedTime(endDt, tz));
+  errors.value = undefined;
+  if (end.value < new Date()) {
+    errors.value = Error(`End date is before today.`);
+  } else if (end.value < start.value)
+    errors.value = Error(`End date is before start date.`);
+};
 
+// const validateEnd = () => {
+//   if (end.value < start.value) throw Error(`End date is before start date`);
+// };
 onMounted(() => {
   store.getReservations();
 });
@@ -35,10 +62,54 @@ onMounted(() => {
     </header>
     <main class="overflow-auto">
       <p>
-        The table below lists all upcoming reservations. Past or cancelled
-        reservations are not shown. Reservations that are currently active are
+        The table below lists reservations filtered by start and end dates shown
+        below. Past or cancelled reservations are not shown. Reservations that
+        are currently active are
         <Box as="span" color="blue" variant="light">highlighted in blue</Box>.
+        You can refine this list by changing the start and end dates.
       </p>
+      <section class="gap-4 my-8" v-if="errors">
+        <Message
+          color="orange"
+          variant="light"
+          summary="Invalid filters selection"
+        >
+          {{ errors.message }}
+        </Message>
+      </section>
+      <section class="flex gap-4 my-8">
+        <Entry
+          id="start"
+          label="Start"
+          required
+          :inline="false"
+          v-slot="{ id, required }"
+        >
+          <Input
+            :id="id"
+            :required="required"
+            type="date"
+            :modelValue="format(toZonedTime(start, tz), 'yyyy-MM-dd')"
+            @changed="validateStart"
+          />
+        </Entry>
+        <Entry
+          id="end"
+          label="End"
+          required
+          :inline="false"
+          v-slot="{ id, required }"
+        >
+          <Input
+            :id="id"
+            :required="required"
+            type="date"
+            :modelValue="format(toZonedTime(end, tz), 'yyyy-MM-dd')"
+            @changed="validateEnd"
+          />
+        </Entry>
+      </section>
+
       <table class="-ml-2 w-full table-auto border-separate border-spacing-y-1">
         <caption></caption>
         <thead class="pb-4">
@@ -54,7 +125,7 @@ onMounted(() => {
         <tbody>
           <Box
             as="tr"
-            v-for="res in store.reservations.sort(
+            v-for="res in reservations.sort(
               (a, b) => a.start.valueOf() - b.start.valueOf()
             )"
             :key="res.id"
