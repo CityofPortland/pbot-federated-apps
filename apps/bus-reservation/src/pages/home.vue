2c6e93bc1dfe975f414f456e5b81c0a14864fd39
Jenny Oh <jenny.oh@portlandoregon.gov>
2025-06-30 16:02:59 -0700

    feat: Show app offline msg with redirect link to TSUP
    ref: PCM-1208

--- apps/bus-reservation/src/pages/home.vue
+++ apps/bus-reservation/src/pages/home.vue
@@ -16,6 +16,18 @@ import { Box } from '@pbotapps/components';
           accepted by PBOT.
         </p>
       </div>
+      <div class="mx-auto flex flex-col gap-4 prose-lg bg-lime-400">
+        <p>
+          As of July 1st, 2025, all bus parking reservations will be managed by
+          the Temporary Street Use Permitting (TSUP) group. For more information
+          on requesting a permit, please visit the link below.
+        </p>
+        <a
+          href="https://www.portland.gov/transportation/permitting/temporary-street-use-permitting-tsup#toc-on-street-bus-or-motorcoach-parking"
+          class="text-blue-400"
+          >Click here to continue</a
+        >
+      </div>
       <div class="mx-auto grid grid-cols-3 gap-12"></div>
     </Box>
   </article>
