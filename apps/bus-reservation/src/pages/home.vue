706c64aeb8c67ff9bc41e2f7c084a6ea1e53b6c1
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2025-07-03 15:54:37 +0000

    refactor: Use message component for redirection message
    ref: PCM-1208

--- apps/bus-reservation/src/pages/home.vue
+++ apps/bus-reservation/src/pages/home.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Box } from '@pbotapps/components';
+import { Anchor, Box, Message } from '@pbotapps/components';
 </script>
 
 <template>
@@ -8,27 +8,28 @@ import { Box } from '@pbotapps/components';
       as="header"
       class="p-12 grid grid-cols-1 items-center justify-items-center gap-12"
     >
-      <div class="mx-auto flex flex-col gap-4 prose-lg">
-        <h1>Welcome to the Bus Reservation application!</h1>
-        <p>
-          This application is intended for use by permitted users to validate
-          that requests for exclusive use of bus parking zones have been
-          accepted by PBOT.
-        </p>
-      </div>
-      <div class="mx-auto flex flex-col gap-4 prose-lg bg-lime-400">
+      <Message
+        color="blue"
+        variant="light"
+        icon="information"
+        summary="Bus Parking Reservations Transitioned to TSUP"
+        border
+        rounded
+        class="p-6"
+      >
         <p>
           As of July 1st, 2025, all bus parking reservations will be managed by
           the Temporary Street Use Permitting (TSUP) group. For more information
           on requesting a permit, please visit the link below.
         </p>
-        <a
-          href="https://www.portland.gov/transportation/permitting/temporary-street-use-permitting-tsup#toc-on-street-bus-or-motorcoach-parking"
-          class="text-blue-400"
-          >Click here to continue</a
-        >
-      </div>
-      <div class="mx-auto grid grid-cols-3 gap-12"></div>
+        <div>
+          <Anchor
+            href="https://www.portland.gov/transportation/permitting/temporary-street-use-permitting-tsup#toc-on-street-bus-or-motorcoach-parking"
+          >
+            Click here to continue
+          </Anchor>
+        </div>
+      </Message>
     </Box>
   </article>
 </template>
